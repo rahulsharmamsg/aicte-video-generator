7b8313b2c2877d937064ae76b85931c306a02e94
uineat <108052879+uineat@users.noreply.github.com>
2024-02-05 17:30:53 +0530

    feat-new-features

--- src/Components/HomeSlider.jsx
+++ src/Components/HomeSlider.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom'
-import homeSliderImg from '../assets/images/homeSlider.jpg';
+import homeSliderImg from '../assets/images/slider01.jpg';
+import { Article, Book, BookOpen, ChalkboardTeacher, CheckSquare, Command, Exam, FinnTheHuman, Note, Notebook, Question } from "phosphor-react";
 
 function HomepageSlider() {
   const scrollToTop = () => {
@@ -16,6 +17,17 @@ function HomepageSlider() {
         <Link to="">
          <img src={homeSliderImg} alt="" />
          </Link>
+         <div className="sliderLink">
+          <Link className="videoBtn sBtn1"><span><Notebook className="lnkIcn" size={24} /> Create a Book</span></Link>
+          <Link className="videoBtn sBtn2"><span><Note className="lnkIcn" size={24} /> Create Notes</span></Link>
+          <Link className="videoBtn sBtn3"><span><ChalkboardTeacher className="lnkIcn" size={24} /> Create a Course</span></Link>
+          <Link className="videoBtn sBtn4"><span><CheckSquare className="lnkIcn" size={24} /> Create an Assessment</span></Link>
+          <Link className="videoBtn sBtn5"><span><Question className="lnkIcn" size={24} /> Create Question & Answer Paper</span></Link>
+          <Link className="videoBtn sBtn6"><span><Exam className="lnkIcn" size={24} /> Create Exam Preparation Modules</span></Link>
+          <Link className="videoBtn sBtn7"><span><FinnTheHuman className="lnkIcn" size={24} /> Create Talking Avatars</span></Link>
+          <Link className="videoBtn sBtn8"><span><Command className="lnkIcn" size={24} /> Create Quizzes</span></Link>
+          <Link className="videoBtn sBtn9"><span><Article className="lnkIcn" size={24} /> Create a Blog</span></Link>
+         </div>
       </section>
       
     </>
